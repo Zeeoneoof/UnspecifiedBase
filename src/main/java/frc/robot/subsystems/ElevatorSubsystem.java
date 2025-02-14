c8818f73a29027a49262bc358a7aec4fcf29d668
Crystallized <lolxd4147@gmail.com>
2025-02-13 22:25:45 -0500

    Configure elevator motor

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,13 +14,23 @@ public class ElevatorSubsystem extends SubsystemBase {
     private final SparkMax m_elevatorMotor = new SparkMax(Constants.Elevator.kElevatorMotorPort, MotorType.kBrushless);
     private final SparkMaxConfig elevatorConfig = new SparkMaxConfig();
     
-    // Config Init
     public ElevatorSubsystem(){
+        // Configure the elevator motor
         elevatorConfig
             .voltageCompensation(12.0)
-            .
+            .idleMode(IdleMode.kBrake)
+            .smartCurrentLimit(40);
+        elevatorConfig.closedLoop
+        .p(0.1)
+        .i(0.0)
+        .d(0.01)
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder);
+
+        m_elevatorMotor.configure(elevatorConfig,ResetMode.kResetSafeParameters,PersistMode.kPersistParameters);
+
             
     }
+    
 
     
 }
